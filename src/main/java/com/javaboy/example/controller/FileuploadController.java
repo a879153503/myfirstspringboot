48515364ea4e2602bd25d342645e4593f718cb54
qiaoperng <879153503@qq.com>
2020-06-28 18:05:31 +0800

    文件上传功能

---
+++ src/main/java/com/javaboy/example/controller/FileuploadController.java
@@ -0,0 +1,45 @@
+package com.javaboy.example.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.UUID;
+
+@RestController
+public class FileuploadController {
+
+
+    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+
+    @PostMapping("/uploads")
+    public String uploads(MultipartFile[] files, HttpServletRequest req){
+
+        String foramt = sdf.format(new Date());
+
+        String realPath = req.getServletContext().getRealPath("/img/")+foramt;
+        File folder = new File(realPath);
+
+        if (!folder.exists()){
+            folder.mkdirs();
+        }
+
+        for (MultipartFile file:files) {
+            String oladName = file.getOriginalFilename();
+            String newName = UUID.randomUUID().toString()+oladName.substring(oladName.lastIndexOf("."));
+            try {
+                file.transferTo(new File(folder,newName));
+                String url = req.getScheme()+"://"+req.getServerName()+":"+req.getServerPort()+"/img/"+foramt+"/"+newName;
+                System.out.println(url);
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+        return "success";
+    }
+}
